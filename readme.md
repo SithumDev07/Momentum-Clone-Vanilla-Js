620a156557db229c5ab7459432935aded6ee6e71
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-10 23:59:26 +0530

    Sending todods in to local storage

--- readme.md
+++ readme.md
@@ -18,3 +18,38 @@
     Then set the image source
     Then append element to the parent using appendChild() funtion
     prepend() funtion will add element to top of the parent
+
+## Deleting element
+
+    Add an event listener and call a function
+    grab event as a parameter
+    using that get the target parentElement
+    then remove the parentElement
+
+    const li = event.target.parentElement;
+    li.remove();
+
+## Save on local storage
+
+    Only text can be saved on localstorage
+    So we can user JSON.stringify(array) function to save the array as a text
+
+## Convert saved text as an array
+
+    JSON.parse(text) using we can convert the array shaped text as an array
+
+## Foreach already giving us the item as a parameter
+
+    todos.forEach(sayHello)
+
+    function sayhello(item) {
+        console.log("Hello", item);
+    }
+
+    Since that parameter already supplying, we can simplify this,
+
+    parsedTodos.forEach((element) => paintTodo(element));
+
+    into,
+
+    parsedTodos.forEach(paintTodo);
--- todo.js
+++ todo.js
@@ -1,6 +1,14 @@
 const todoList = document.querySelector("#todo-list");
 const todoInput = document.querySelector("#todo-form input");
 
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function deletTodo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -8,13 +16,14 @@ function deletTodo(event) {
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "🤏";
   button.addEventListener("click", deletTodo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   todoList.appendChild(li);
 }
 
@@ -22,7 +31,21 @@ function onSubmitForm(e) {
   e.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  paintTodo(newTodo);
+  const newTodoObject = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todos.push(newTodoObject);
+  paintTodo(newTodoObject);
+  saveTodos();
 }
 
 document.querySelector("#todo-form").addEventListener("submit", onSubmitForm);
+
+const savedTodods = localStorage.getItem(TODOS_KEY);
+
+if (saveTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodods);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
